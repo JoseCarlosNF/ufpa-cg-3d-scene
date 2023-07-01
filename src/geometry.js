f014d505db4570c76f766e76b2382c0fe9674537
Jose Noronha <jnoronha@uolinc.com>
2023-07-01 19:05:51 -0300

    refactor(geometry): remove formas nao utilizadas

--- src/geometry.js
+++ src/geometry.js
@@ -11,22 +11,6 @@ export default class CommonGeometryElements {
     this.helper = helper
   }
 
-  cylinder (color="cyan", radiusTop=1, radiusBottom=1, height=2) {
-    const material = new THREE.MeshLambertMaterial({color: color});
-    const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height);
-    const cilynder = new THREE.Mesh(geometry, material);
-    this.scene.add(cilynder)
-    return cilynder
-  }
-
-  cube(color="green", width=1, height=1, depth=1) {
-    const material = new THREE.MeshBasicMaterial({color: color});
-    const geometry = new THREE.BoxGeometry(width, height, depth);
-    const cube = new THREE.Mesh(geometry, material);
-    this.scene.add(cube);
-    return cube
-  }
-
   sphere(color="orange", radius=1, widthSegments=20, heightSegments=20) {
     const material = new THREE.MeshLambertMaterial({color: color, map: basketball_texture})
     const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments)
